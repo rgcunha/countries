3babf9e22c91a500b1b60f0bb0aa26a2b38d4ca7
Ruben Cunha <ruben.cunha@gmail.com>
2017-04-28 00:02:42 +0200

    Fetch countries with componentWillMount hook

--- src/routes/Countries/components/FilterableCountries.js
+++ src/routes/Countries/components/FilterableCountries.js
@@ -3,13 +3,21 @@ import PropTypes from 'prop-types'
 import { Countries } from './Countries'
 import { SearchBar } from './SearchBar'
 
-export const FilterableCountries = (props) => (
-  <div className="filterable-countries">
-    <h1>Countries</h1>
-    <SearchBar fetch={props.fetchCountries}></SearchBar>
-    <Countries countries={props.countries}></Countries>
-  </div>
-)
+class FilterableCountries extends React.Component {
+  componentWillMount() {
+    this.props.fetchCountries();
+  }
+
+  render() {
+    return (
+      <div className="filterable-countries">
+        <h1>Countries</h1>
+        <SearchBar fetch={this.props.fetchCountries}></SearchBar>
+        <Countries countries={this.props.countries}></Countries>
+      </div>
+    )
+  }
+}
 
 FilterableCountries.propTypes = {
   fetchCountries    : PropTypes.func.isRequired,
